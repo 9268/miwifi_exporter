eea38d39542fa86409838aad8696757a7b4b2b61
9268 <83506876@qq.com>
2022-08-26 00:27:30 +0800

    Update collector.go
    fix typo

--- collector/collector.go
+++ collector/collector.go
@@ -147,7 +147,7 @@ func (c *Metrics) Collect(ch chan<- prometheus.Metric) {
 				break
 			}
 		}
-		if devIp!="" && devIsAP!=""{ // 仅在设备在线时上传
+		if devIP!="" && devIsAP!=""{ // 仅在设备在线时上传
 			ch <- prometheus.MustNewConstMetric(c.metrics["device_upload_traffic"], prometheus.GaugeValue, devUpload, devIP, devMac, devName, devIsAP)
 			ch <- prometheus.MustNewConstMetric(c.metrics["device_download_traffic"], prometheus.GaugeValue, devDownload, devIP, devMac, devName, devIsAP)
 			ch <- prometheus.MustNewConstMetric(c.metrics["device_upload_speed"], prometheus.GaugeValue, devUpSpeed, devIP, devMac, devName, devIsAP)
